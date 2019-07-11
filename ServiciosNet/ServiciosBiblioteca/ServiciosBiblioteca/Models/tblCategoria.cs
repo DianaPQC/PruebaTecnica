2f6c66737adf3f34e66670e5524bc4ae5d59e42f
Diana Quiroga <dianapquirogac@gmail.com>
2019-07-11 07:29:43 -0500

    Paso

---
+++ ServiciosNet/ServiciosBiblioteca/ServiciosBiblioteca/Models/tblCategoria.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ServiciosBiblioteca.Models
+{
+    //[Table("tblCategoria", Schema ="dbo")]
+    
+    public class tblCategoria
+    {
+        [Key]
+        public int? idCategoria { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string nombre { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string descripcion { get; set; }
+    }
+}
